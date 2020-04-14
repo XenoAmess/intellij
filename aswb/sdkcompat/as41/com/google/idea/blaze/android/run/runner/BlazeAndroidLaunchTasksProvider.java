c056f4aa59d22eee35794793f46f61e29d159057
Googler <intellij-github@google.com>
2020-04-13 22:56:00 -0700

    Remove obsolete compat classes and duplicates.
    Many sdkcompat classes aren't required anymore since 3.5 is obsolete.
    This CL cleans up these duplicate compat classes and bring them out of
    sdkcompat.
    
    PiperOrigin-RevId: 306379770

--- aswb/sdkcompat/as41/com/google/idea/blaze/android/run/runner/BlazeAndroidLaunchTasksProvider.java
+++ aswb/sdkcompat/as41/com/google/idea/blaze/android/run/runner/BlazeAndroidLaunchTasksProvider.java
@@ -39,7 +39,6 @@ import com.android.tools.idea.run.util.ProcessHandlerLaunchStatus;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import com.google.idea.blaze.android.run.LaunchStatusCompat;
 import com.google.idea.blaze.android.run.binary.UserIdHelper;
 import com.intellij.execution.ExecutionException;
 import com.intellij.openapi.diagnostic.Logger;
@@ -89,8 +88,7 @@ public class BlazeAndroidLaunchTasksProvider implements LaunchTasksProvider {
       packageName = applicationIdProvider.getPackageName();
     } catch (ApkProvisionException e) {
       LOG.error(e);
-      LaunchStatusCompat.terminateLaunch(
-          launchStatus, "Unable to determine application id: " + e, true);
+      launchStatus.terminateLaunch("Unable to determine application id: " + e, true);
       return ImmutableList.of();
     }
 
@@ -159,11 +157,10 @@ public class BlazeAndroidLaunchTasksProvider implements LaunchTasksProvider {
       }
     } catch (ApkProvisionException e) {
       LOG.error(e);
-      LaunchStatusCompat.terminateLaunch(
-          launchStatus, "Unable to determine application id: " + e, true);
+      launchStatus.terminateLaunch("Unable to determine application id: " + e, true);
       return ImmutableList.of();
     } catch (ExecutionException e) {
-      LaunchStatusCompat.terminateLaunch(launchStatus, e.getMessage(), true);
+      launchStatus.terminateLaunch(e.getMessage(), true);
       return ImmutableList.of();
     }
 
@@ -212,7 +209,7 @@ public class BlazeAndroidLaunchTasksProvider implements LaunchTasksProvider {
     try {
       return runContext.getDebuggerTask(androidDebugger, androidDebuggerState, packageIds);
     } catch (ExecutionException e) {
-      LaunchStatusCompat.terminateLaunch(launchStatus, e.getMessage(), true);
+      launchStatus.terminateLaunch(e.getMessage(), true);
       return null;
     }
   }
